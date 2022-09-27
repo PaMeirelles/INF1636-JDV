a787980fec4588e150e4416c909c9a6ff432585a
Rafael Paladini <56520474+PaMeirelles@users.noreply.github.com>
2022-09-27 11:23:27 +0000

    game_is_over (nao testado)

--- src/regras/CtrlRegras.java
+++ src/regras/CtrlRegras.java
@@ -18,4 +18,38 @@ public class CtrlRegras {
 	public int getVez() {
 		return vez;
 	}	
+  
+  public boolean square_is_valid(int x, int y){
+    return tabuleiro[x][y] == 0;
+  }
+
+  public game_is_over(){
+    int result;
+    // Horizontal
+    for(int i=0; i < 3; i++){
+      result = tabuleiro[i][0] + tabuleiro[i][1] + tabuleiro[i][2];
+      if(result !=0){
+        return result / 3;
+      }
+    }
+
+    // Vertical
+    for(int j=0; j < 3; j++){
+      result = tabuleiro[0][j] + tabuleiro[1][j] + tabuleiro[2][j];
+      if(result != 0){
+        return result / 3;
+      }
+    }
+
+    // Diagonais
+    if(tabuleiro[0][0] == tabuleiro[1][1] == tabuleiro[2][2]){
+      return tabuleiro[0][0];
+    }
+
+    if(tabuleiro[2][0] == tabuleiro[1][1] == tabuleiro[0][2]){
+      return tabuleiro[1][1];
+    }
+    return 0;
+  }
+  
 }
